69cb81d4065f91188320ef4828eeee6a61fd2726
Ratheesh <ratheesh.r@tesark.in>
2022-07-02 17:13:05 +0530

    added new things

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+// npm start for the server
+
 const lodash = require("lodash");
 
 const items = [1, [2, [3, [4]]]];
 const newItems = lodash.flattenDeep(items);
 console.log(newItems);
+
+// npm start for the server
